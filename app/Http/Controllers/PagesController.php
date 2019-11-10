cd4e9a40cda1b0b1a9c46fb121c847e7bb16fc48
heriher76 <herhermawan007@gmail.com>
2019-11-10 15:55:33 +0700

    updatevisimisi2

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\News;
+use App\Gallery;
+use App\VisiMisi;
+
+class PagesController extends Controller
+{
+    public function home()
+    {
+    	return view('pages.home');
+    }
+    public function profile()
+    {
+    	return view('pages.profile');
+    }
+    public function galleryPhoto()
+    {
+        $galleries = Gallery::where('type', 'image')->orderBy('updated_at', 'desc')->paginate(5);
+
+    	return view('pages.gallery-photo', compact('galleries'));
+    }
+    public function galleryVideo()
+    {
+        $galleries = Gallery::where('type', 'video')->orderBy('updated_at', 'desc')->paginate(5);
+
+        return view('pages.gallery-video', compact('galleries'));
+    }
+    public function news()
+    {
+        $news = News::orderBy('updated_at', 'desc')->where('publish_status', 1)->paginate(5);
+
+        $oldNews = News::orderBy('updated_at', 'asc')->where('publish_status', 1)->limit(5)->get(); 
+
+    	return view('pages.news', compact('news', 'oldNews'));
+    }
+    public function workProgram()
+    {
+    	return view('pages.work-program');
+    }
+    public function service()
+    {
+    	return view('pages.service');
+    }
+    public function visiMisi() 
+    {
+        $visiMisi = VisiMisi::first();
+
+        return view('pages.profile.visi-misi', compact('visiMisi'));
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+
+Route::get('/', 'PagesController@home');
+Route::get('/visi-misi', 'PagesController@visiMisi');
+Route::get('/structure-organization', 'PagesController@structureOrganization');
+Route::get('/gallery-photo', 'PagesController@galleryPhoto');
+Route::get('/gallery-video', 'PagesController@galleryVideo');
+Route::get('/news', 'PagesController@news');
+Route::get('/work-program', 'PagesController@workProgram');
+Route::get('/service', 'PagesController@service');
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+	Route::get('/', 'Admin\AdminPagesController@index');
+	Route::resource('/users', 'Admin\UserAdminController');
+	Route::resource('/gallery', 'Admin\GalleryAdminController');
+	Route::resource('/news', 'Admin\NewsAdminController');
+	Route::resource('/visi-misi', 'Admin\VisiMisiAdminController');
+	Route::resource('/structure-organization', 'Admin\StructureOrganizationAdminController');
+});
+
+Route::get('/{slug}', 'NewsController@show');
