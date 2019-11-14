ccf288d70eeb9908059d4370c5498326648dfd22
Heri Hermawan <herhermawan007@gmail.com>
2019-11-14 09:04:51 +0700

    updatefooter

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -3,7 +3,8 @@
         <div class="row justify-content-between">
             <div class="col-sm-6 col-md-4 col-xl-3">
                 <div class="single-footer-widget footer_1">
-                    <a href="index.html"> <!-- <img src="img/logo.png" alt=""> --> </a>
+                    <h4>Tentang</h4>
+                    <a href="#"> <!-- <img src="img/logo.png" alt=""> --> </a>
                     <p>But when shot real her. Chamber her one visite removal six
                         sending himself boys scot exquisite existend an </p>
                     <p>But when shot real her hamber her </p>
@@ -11,36 +12,25 @@
             </div>
             <div class="col-sm-6 col-md-4 col-xl-4">
                 <div class="single-footer-widget footer_2">
-                    <h4>Newsletter</h4>
+                    <h4>Menu</h4>
                     <p>Jika Anda tertarik untuk belajar lebih
                         banyak dan membantu anak Anda
                         atau diri Anda mencapai tingkat
                         kehidupan selanjutnya, daftarkan diri
                         Anda untuk surat kabar kami.
                     </p>
-                    <form action="#">
-                        <div class="form-group">
-                            <div class="input-group mb-3">
-                                <input type="text" class="form-control" placeholder='Enter email address'
-                                    onfocus="this.placeholder = ''"
-                                    onblur="this.placeholder = 'Enter email address'">
-                                <div class="input-group-append">
-                                    <button class="btn btn_1" type="button"><i class="ti-angle-right"></i></button>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
+                    
                     <div class="social_icon">
                         <a href="#"> <i class="ti-facebook"></i> </a>
                         <a href="#"> <i class="ti-twitter-alt"></i> </a>
                         <a href="#"> <i class="ti-instagram"></i> </a>
-                        <a href="#"> <i class="ti-skype"></i> </a>
+                        <a href="#"> <i class="ti-youtube"></i> </a>
                     </div>
                 </div>
             </div>
             <div class="col-xl-3 col-sm-6 col-md-4">
                 <div class="single-footer-widget footer_2">
-                    <h4>Contact us</h4>
+                    <h4>Hubungi Kami</h4>
                     <div class="contact_info">
                         <p><span> Address :</span> Hath of it fly signs bear be one blessed after </p>
                         <p><span> Phone :</span> +2 36 265 (8060)</p>
